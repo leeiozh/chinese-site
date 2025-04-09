b5c5a0d2b69b692ec8b1e9af3d191fb47a322fb9
leeiozh <ezhova.ea@phystech.edu>
2025-04-09 14:43:00 +0300

    add docs to forms.py

--- chinese/forms.py
+++ chinese/forms.py
@@ -1,12 +1,18 @@
+"""Forms for managing Chinese character input and practice."""
+
 from django import forms
 from .models import Character
 
 
 class CharacterForm(forms.ModelForm):
+    """Form for adding or editing a Chinese character."""
+
     class Meta:
         model = Character
         fields = ['character', 'pinyin', 'translation']
 
 
 class PracticeForm(forms.Form):
+    """Form for practicing character translation."""
+
     translation = forms.CharField(label='Translation', max_length=100)
