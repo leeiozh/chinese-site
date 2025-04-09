98b540fdaa256513ab6b82e20e26709b8d39befe
leeiozh <ezhova.ea@phystech.edu>
2025-04-09 14:42:27 +0300

    add docs to models.py

--- chinese/models.py
+++ chinese/models.py
@@ -1,12 +1,17 @@
+"""Defines the Character model for storing Chinese characters and statistics."""
+
 from django.db import models
 
 
 class Character(models.Model):
+    """Represents a Chinese character with pinyin, translation, and usage statistics."""
+
     character = models.CharField(max_length=100)
     pinyin = models.CharField(max_length=100)
     translation = models.CharField(max_length=100)
     correct_count = models.PositiveIntegerField(default=0)
     incorrect_count = models.PositiveIntegerField(default=0)
 
-    def __str__(self):
-        return self.character
+    def __str__(self) -> str:
+        """Return the character as string representation."""
+        return str(self.character)
