64e48b411f67128f205a2dfada4e596735b3216d
leeiozh <ezhova.ea@phystech.edu>
2025-04-09 14:45:46 +0300

    add docs to manage.py, urls.py

--- chinese/urls.py
+++ chinese/urls.py
@@ -1,3 +1,5 @@
+"""URL configuration for the Chinese learning application."""
+
 from django.urls import path
 from . import views
 
--- manage.py
+++ manage.py
@@ -1,8 +1,11 @@
+"""Management script for Django project."""
+
 import os
 import sys
 
 
 def main():
+    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chinese_site.settings')
     try:
         from django.core.management import execute_from_command_line
